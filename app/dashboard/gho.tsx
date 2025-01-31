a62fbe6d5df204fdcc8e0e927aa2ad20dcc1700b
steven8306 <stevenjohn201315@gmail.com>
2025-01-31 08:54:18 -0800

    fix bug

---
+++ app/dashboard/gho.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const gho = () => {
+  return (
+    <div>gho</div>
+  )
+}
+
+export default gho
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,8 @@ import { Card } from '@/app/dashboard/cards';
 import RevenueChart from '@/app/dashboard/revenue-chart';
 import LatestInvoices from '@/app/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/font';
- import {fetchRevenue} from '@/app/lib/data'
+ 
 export default async function Page() {
-    const revenue = await fetchRevenue();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -21,7 +20,7 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+        {/* <RevenueChart revenue={revenue}  /> */}
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
